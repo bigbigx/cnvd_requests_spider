efa0d782fe6d112e7bd51e74a3ac9fd6387005e8
RyQcan <13723926068@163.com>
2019-06-06 00:44:25 +0800

    fix

--- cnvd2.py
+++ cnvd2.py
@@ -11,8 +11,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, TEXT, Index, DATE
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
-from .a import COOKIES
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import ast
+def COOKIES():
+    chrome_options = Options()
+    # 加上下面两行，解决报错
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get("https://www.cnvd.org.cn/flaw/list.htm?max=20&offset=2050")
+    cj = driver.get_cookies()
+    cookie = ''
+    for c in cj:
+        # if (c==cj[-1]):
+        #     cookie += "'"+c['name'] + "':'" + c['value'] + "'"
+        # else:
+        cookie += "'"+c['name'] + "':'" + c['value'] + "',"
+    cookie=ast.literal_eval('{'+cookie+'}')
+    driver.quit()
+    return cookie
+    
+    
 engine = create_engine(
     "mysql+pymysql://root:root@localhost/scrapy?charset=utf8", max_overflow=5)
 Base = declarative_base()
